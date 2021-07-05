b47df00079cec55e20a95067132f8e280dc9d0ee
Kostya <Kpstya@mail.ru>
2021-07-05 18:47:11 +0300

    исправил авторизацию

--- engine/pages/auth.php
+++ engine/pages/auth.php
@@ -27,45 +27,63 @@ function loginAction()
     $login = clearStr($_POST['login']);
     $password = $_POST['password'];
 
-    $sql = "SELECT login, password FROM users WHERE login = '{$login}'";   //<--создание запроса к БД
-    $result = mysqli_query(getLink(), $sql);    //<--отправка запроса к БД и запись результата запроса в переменную
-    $userData = mysqli_fetch_assoc($result);    //<--обработка результата запроса
+    $sqlAllUsers = "SELECT login FROM users";
+    $resultAllUsers = mysqli_query(getLink(), $sqlAllUsers);
+    $allUsersData = mysqli_fetch_all($resultAllUsers);
 
-    $sqlAllData = "SELECT id, name, is_admin, position, avatar FROM users WHERE login = '{$login}'";   //<--создание запроса к БД(поиск имени)
-    $resultAllData = mysqli_query(getLink(), $sqlAllData);
-    $allData = mysqli_fetch_assoc($resultAllData);
+    $arr = [];
 
-    $dataId = '';
-    $dataName = '';
-    $dataIsAdmin = '';
-    $dataPosition = '';
-    $dataAvatar = '';
-
-    foreach ($allData as $item => $data) {
-        if ($item == 'id') {
-            $dataId = $data;
-        } elseif ($item == 'name') {
-            $dataName = $data;
-        } elseif ($item == 'is_admin') {
-            $dataIsAdmin = $data;
-        } elseif ($item == 'position') {
-            $dataPosition = $data;
-        } elseif ($item == 'avatar') {
-            $dataAvatar = $data;
+    foreach ($allUsersData as $item) {
+        foreach ($item as $data) {
+            $arr[] = $data;
         }
     }
 
-    if (!empty($userData) && password_verify($password, $userData['password'])) {    //<--верификация хэша пароля
-        $_SESSION['user'] = $login;
-        $_SESSION['id'] = $dataId;
-        $_SESSION['name'] = $dataName;
-        $_SESSION['is_admin'] = $dataIsAdmin;
-        $_SESSION['position'] = $dataPosition;
-        $_SESSION['avatar'] = $dataAvatar;
-    }
+    if (in_array($login, $arr)) {
 
-    header('Location: /?p=personalPage');
-    return;
+        $sql = "SELECT login, password FROM users WHERE login = '{$login}'";   //<--создание запроса к БД
+        $result = mysqli_query(getLink(), $sql);    //<--отправка запроса к БД и запись результата запроса в переменную
+        $userData = mysqli_fetch_assoc($result);    //<--обработка результата запроса
+
+        $sqlAllData = "SELECT id, name, is_admin, position, avatar FROM users WHERE login = '{$login}'";   //<--создание запроса к БД(поиск имени)
+        $resultAllData = mysqli_query(getLink(), $sqlAllData);
+        $allData = mysqli_fetch_assoc($resultAllData);
+
+        $dataId = '';
+        $dataName = '';
+        $dataIsAdmin = '';
+        $dataPosition = '';
+        $dataAvatar = '';
+
+        foreach ($allData as $item => $data) {
+            if ($item == 'id') {
+                $dataId = $data;
+            } elseif ($item == 'name') {
+                $dataName = $data;
+            } elseif ($item == 'is_admin') {
+                $dataIsAdmin = $data;
+            } elseif ($item == 'position') {
+                $dataPosition = $data;
+            } elseif ($item == 'avatar') {
+                $dataAvatar = $data;
+            }
+        }
+
+        if (!empty($userData) && password_verify($password, $userData['password'])) {    //<--верификация хэша пароля
+            $_SESSION['user'] = $login;
+            $_SESSION['id'] = $dataId;
+            $_SESSION['name'] = $dataName;
+            $_SESSION['is_admin'] = $dataIsAdmin;
+            $_SESSION['position'] = $dataPosition;
+            $_SESSION['avatar'] = $dataAvatar;
+        }
+
+        header('Location: /?p=personalPage');
+        return;
+    } else {
+        header('Location: /?p=personalPage');
+        return;
+    }
 }
 
 function outAction()
